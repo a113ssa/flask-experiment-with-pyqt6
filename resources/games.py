aad581fdadb640b5427bbbbf5ca60bfd8abb17a5
a113ssa <alessa.silent.hill.insider@gmail.com>
2025-02-20 21:54:04 +0400

    Add Langchain

--- resources/games.py
+++ resources/games.py
@@ -2,21 +2,23 @@ from flask.views import MethodView
 from flask_smorest import Blueprint
 
 from schemas import GameRequestSchema, GameResponseSchema
-from src.chatbot.chatbot import GeminiChatbot as chatbot
+from src.chatbot.chatbot import Chatbot
+
+from langchain_core.messages import SystemMessage, HumanMessage
 
 blp = Blueprint("Games", __name__, description="Game operations")
-chat = chatbot()
+chat = Chatbot()
 
 
 @blp.route("/games")
 class Games(MethodView):
-    @blp.response(200, GameResponseSchema)
+    @blp.response(200)#, GameResponseSchema)
     def get(self):
-        response = chat.send_message('Start New Game')
+        response = chat.send_message([SystemMessage('Start New Game')])
         return response
 
     @blp.arguments(GameRequestSchema)
     @blp.response(200, GameResponseSchema)
     def post(self, request_data):
-        response = chat.send_message(request_data['message'])
+        response = chat.send_message([HumanMessage(request_data['message'])])
         return response
--- src/chatbot/chatbot.py
+++ src/chatbot/chatbot.py
@@ -2,9 +2,11 @@ import json
 import os
 from typing import Literal
 
-import google.generativeai as genai  # type: ignore
 import i18n  # type: ignore
 import typing_extensions as typing
+
+from langchain_google_genai import GoogleGenerativeAI
+from langchain_core.messages import SystemMessage, HumanMessage
 
 
 class GameResponse(typing.TypedDict):
@@ -16,19 +18,13 @@ class GameResponse(typing.TypedDict):
 i18n.load_path.append('src/locales')
 i18n.set('locale', os.environ['LOCALE'])
 
-class GeminiChatbot:
+class Chatbot:
     def __init__(self):
-        genai.configure(api_key=os.environ['GOOGLE_API_KEY'])
-        self.model = genai.GenerativeModel(
-            os.environ['GEMINI_VERSION'],
-            generation_config=genai.GenerationConfig(
-                response_mime_type='application/json',
-                response_schema=GameResponse
+        self.model = GoogleGenerativeAI(
+            model=os.environ['LLM_VERSION'],
+            temperature=os.environ['LLM_TEMPERATURE'],
             )
-        )
-        self.chat = self.model.start_chat(history=[])
-        self.chat.send_message(i18n.t('game.init_game'))
+        self.model.invoke([SystemMessage(i18n.t('game.init_game'))])
 
     def send_message(self, message):
-        response = self.chat.send_message(message)
-        return json.loads(response.text)
+        return self.model.invoke(message)
